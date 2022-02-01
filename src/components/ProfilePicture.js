65b85c7754e5d0505f069b983c78153f2be2e9f1
JesperHarders <4160144@rts-365.dk>
2022-02-01 12:28:50 +0100

    Add profile picture

---
+++ src/components/ProfilePicture.js
@@ -0,0 +1,10 @@
+const ProfilePicture = () => {
+    return (
+        <div className="bg-white w-fit p-1 rounded-full relative">
+            <img className="rounded-full" src="https://via.placeholder.com/50" alt=""/>
+            <div className="w-4 h-4 bg-[#FF9898] rounded-full absolute top-0 right-0"></div>
+        </div>
+    );
+}
+ 
+export default ProfilePicture;
